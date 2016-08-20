f7657ba09f0d9d0791992667f609fae6791c4d8d
Yuki Yoshida <y8yoshida@gmail.com>
2016-08-20 13:00:30 +0900

    add example-readdata

--- example-dataread/js/main.js
+++ example-dataread/js/main.js
@@ -1,19 +1,29 @@
 function pictureHappinessOnEarth(){
 	setCenter([pheCoords.shizuoka[0], 0]);
 	makeOceanColor("black");
-//	drawBaseMap({fillColor: "white"});
+	drawBaseMap({fillColor: "white"});
 
-	var jpNum = countryList.getDataFromName("japan").ISO31661_numeric;
-	drawCountry(jpNum, {fillColor: "red"});
-	readData("honey.csv",function(data){
-		var dataRange = getDataRange(data, "DATA");
+	var csvfilename = "energy.csv";
+
+	var dataCell = "2001";
+	var countryCell = "Country Code"
+
+	readData(csvfilename,function(data){
+		var dataRange = getDataRange(data, dataCell);
 
 		for(var i=0; i<data.length; i++){
-			var countryCode = data[i]["ISO31661_alpha2"];
-			var countryNum = countryList.getDataFromISOAlpha2(countryCode).ISO31661_numeric;
-			var score = data[i]["DATA"];
+
+			var countryCode = data[i][countryCell];
+
+			var countryNum = countryList.getDataFromISOAlpha3(countryCode).ISO31661_numeric;
+			//var countryNum = countryList.getDataFromISOAlpha2(countryCode).ISO31661_numeric;
+			//var countryNum = countryList.getDataFromName(countryCode).ISO31661_numeric;
+
+			var score = data[i][dataCell];
+
 			var normalizedScore = mapValue(score, dataRange.min, dataRange.max, 0.05, 1);
-			drawCountry(countryNum, {fillColor: rgba(255, 255, 0, normalizedScore)});
+
+			drawCountry(countryNum, {fillColor: rgba(255, 0, 0, normalizedScore)});
 
 		}
 	})
