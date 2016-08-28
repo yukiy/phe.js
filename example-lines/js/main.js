e12195f75957bc513f02967c4e0589bdfd0017fc
Yuki Yoshida <y8yoshida@gmail.com>
2016-08-29 03:20:14 +0900

    update example-lines

--- example-lines/js/main.js
+++ example-lines/js/main.js
@@ -3,31 +3,30 @@ function pictureHappinessOnEarth(){
 	makeOceanColor(rgb(10,10,30));
 
 	drawBaseMap({fillColor: rgb(50,50,80)});
+	drawBorders();
 
 	var startLngLat;
 	var goalLngLat;
 
-	for(var j in pheCoords){
-		startLngLat = pheCoords[j];
-		console.log(startLngLat);
+	var all = getAllCountriesData();
 
-		for(var i=0; i<countryList.data.length; i++){
-			var country = countryList.data[i];
-			if(country.longitude != "" || country.longitude != undefined){
-				goalLngLat = [country.longitude, country.latitude];
+	var randomName = all[Math.floor(random(all.length))].ISO31661_name;
+	var startLngLat = getLngLat(randomName);
 
-				var lineOptions = {
-					strokeWidth : "0.3px",
-					strokeColor : rgb(Math.random()*255,Math.random()*255,Math.random()*255),
-					strokeOpacity : "0.5"
-				}
-				drawLine([startLngLat, goalLngLat], lineOptions);				
-			}
-		}		
-	}
+	for(var i=0; i<all.length; i++){
+		var name = all[i].ISO31661_name;
+		goalLngLat = getLngLat(name);
+
+		var lineOptions = {
+			strokeWidth : "0.3px",
+			strokeColor : rgb(random(255), random(255), random(255) ),
+			strokeOpacity : "0.5"
+		}
+		drawLine([startLngLat, goalLngLat], lineOptions);				
+	}		
 
 }
-
+ 
 
 
 /*----------------------------------------------------------*/
